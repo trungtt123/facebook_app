5df1d9d6093fd763366b01dcca58e3b9832ed17d
TrinhHuynhDuc <huynhductrinhhuynh@gmail.com>
2022-12-05 20:49:56 +0700

    ok

--- src/Services/Helper/constant.js
+++ src/Services/Helper/constant.js
@@ -1,5 +1,5 @@
 import {API_URL} from '@env';
-export const REST_API_URL = 'https://a666-2405-4802-11e-1bb0-3871-abfa-c8a9-8dba.ap.ngrok.io/it4788';
+export const REST_API_URL = 'https://2b9e-222-252-10-41.ap.ngrok.io/it4788';
 //export const REST_API_URL = API_URL;
 export const INTERNET_CONNECTION_FAILED = 'Đường truyền của bạn không ổn định';
 export const INTERNET_CONNECTION_SUCCESS = 'Kết nối đã được khôi phục';
