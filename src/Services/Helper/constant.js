25bbb724ad0ae189bae7d01b115dba942b5f93be
huyngo512 <hngo419512@gmail.com>
2023-01-06 16:53:17 +0700

    fix validate

--- src/Services/Helper/constant.js
+++ src/Services/Helper/constant.js
@@ -1,5 +1,5 @@
 import {API_URL} from '@env';
-export const REST_API_URL = 'https://68d8-104-28-217-55.ap.ngrok.io/it4788';
+export const REST_API_URL = 'https://d71c-42-119-191-83.ap.ngrok.io/it4788';
 //export const REST_API_URL = API_URL;
 const INTERNET_CONNECTION_FAILED = 'Bạn đang offline';
 const INTERNET_CONNECTION_SUCCESS = 'Bạn đang online';
